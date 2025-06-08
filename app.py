9f3bdd72404dc8ab57e21c7aebe704d8ba13ddb6
Albert121s <135711223+Albert121s@users.noreply.github.com>
2025-06-08 22:53:20 +0200

    Aktualizacja kodu z wyłączeniem credentials.json

--- app.py
+++ app.py
@@ -14,8 +14,11 @@ from google.oauth2 import service_account
 from google.oauth2 import service_account
 from google.cloud import bigquery
 
-credentials = service_account.Credentials.from_service_account_file("credentials.json")
+from google.oauth2 import service_account
+
+credentials = service_account.Credentials.from_service_account_file("/etc/secrets/credentials.json")
 client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+
 
 
 st.set_page_config(page_title="⚽ Analiza meczów piłkarskich", layout="wide")
